07037acd9adfde64d4e0e8d73045705d3f775584
it-beige <1119514898@qq.com>
2020-06-08 16:59:38 +0800

    add search

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="city">
     <city-header></city-header>
+    <city-search :cities="cities"></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/search'
 export default {
   name: 'city',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo () {
+      this.axios.get('/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
   }
 }
 </script>
 
-<style lang="stylus" scope>
-
-</style>
+<style lang="stylus" scope></style>
--- src/pages/Home/components/Swiper.vue
+++ src/pages/Home/components/Swiper.vue
@@ -41,4 +41,6 @@ export default {
   background: #eee
   .slide-img
     width: 100%
+    height: 100%
+
 </style>
